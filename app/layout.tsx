040afdc2023caccaeec89c250d6c66cd08738e70
Nalin Singh <nalinsingh20@gmail.com>
2024-01-26 22:43:05 +0530

    Update : Basic Clean Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Academix",
-	description: "An Ed-Tech Platform",
+    title: "Academix",
+    description: "A Management System for Insitutions",
 };
 
 export default function RootLayout({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
