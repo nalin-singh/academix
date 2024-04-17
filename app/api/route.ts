d59a099200cbe83111d0c671443f475dba5ec440
Nalin Singh <nalinsingh20@gmail.com>
2024-04-17 10:13:44 +0530

    Update : Clear

--- app/api/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-export async function GET() {
-    return NextResponse.json({
-        "Message ": "Greetings! Succesful user API setup",
-    });
-}
--- app/api/user/route.ts
+++
@@ -1,8 +0,0 @@
-import { dbConnect } from "@/lib/db";
-import { NextResponse } from "next/server";
-
-export const GET = async () => {
-    const database = dbConnect();
-    console.log(database);
-    return NextResponse.json({ Message: "DB Connection Live" });
-};
