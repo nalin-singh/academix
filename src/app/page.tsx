dae55eb41dee09d59593cf15af7e8a482a4b5418
Nalin Singh <nalinsingh20@gmail.com>
2024-04-17 23:26:45 +0530

    Update : Base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Application() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Academix
+    <main className="flex min-h-screen flex-col">
+      <div className="m-auto">Wisdom Academy</div>
     </main>
   );
 }
