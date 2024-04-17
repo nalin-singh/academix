d406c9530e75d2c299a8c0fe281a051abd8d900d
Nalin Singh <nalinsingh20@gmail.com>
2024-04-18 03:02:19 +0530

    Add : Clerk Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { SignInButton, SignedIn, SignedOut, UserProfile } from "@clerk/nextjs";
+
+export const dynamic = "force-dynamic";
+
 export default function Application() {
   return (
     <main className="flex min-h-screen flex-col">
-      <div className="m-auto">Wisdom Academy</div>
+      <div className="m-auto">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserProfile />
+        </SignedIn>
+      </div>
     </main>
   );
 }
