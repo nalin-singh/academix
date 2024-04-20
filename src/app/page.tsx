ce4e60e093727ab7379205c17f37596834f08a65
Nalin Singh <nalinsingh20@gmail.com>
2024-04-20 23:46:26 +0530

    Add : Component Library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
-import { SignInButton, SignedIn, SignedOut, UserProfile } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
 export default function Application(): JSX.Element {
   return (
     <main className="flex min-h-screen flex-col">
-      <div className="m-auto">
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserProfile />
-        </SignedIn>
-      </div>
+      <nav className="flex items-center justify-between">
+        <p className="text-xl font-bold">Wisdom Academy</p>
+        <div>
+          <SignedOut>
+            <SignInButton>
+              <button>Hello World</button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+      <section className=""></section>
     </main>
   );
 }
